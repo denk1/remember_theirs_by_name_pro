af5464a3e9f46fae92bfad443567d8c79d6ea635
Your Name <tu214@yandex.ru>
2019-04-25 18:03:56 +0300

    data input refactoring

--- remember_theirs_by_name_pro/data.py
+++ remember_theirs_by_name_pro/data.py
@@ -10,58 +10,6 @@ from remember_theirs_by_name_pro.models import Person, \
 
 address_data = [Region, District, Locality]
 
-def fill_new_line(post_obj):
-    surname = post_obj.get("surname")
-    surname_distortion = post_obj.get("surname_distortion")
-    name = post_obj.get("name")
-    name_distortion = post_obj.get("name_distortion")
-    father_name = post_obj.get("father_name")
-    father_name_distortion = post_obj.get("father_name_distortion")
-    birthday = post_obj.get("birthday")
-    data_mobilization = post_obj.get("date_mobilization")
-    born_region_name = post_obj.get("born_region_name")
-    born_district_name = post_obj.get("born_district_name")
-    born_locality_name = post_obj.get("born_locality_name")
-    born_address = [born_region_name, born_district_name, born_locality_name]
-    
-    live_region_name = post_obj.get("live_region_name")
-    live_district_name = post_obj.get("live_district_name")
-    live_locality_name = post_obj.get("live_locality_name")
-    live_address = [live_region_name, live_district_name, live_locality_name]
-    
-    region_military_enlistment_office = post_obj.get("region_region_military_enlistment_office")
-    region_military_enlistment_office = Region.objects.create(region_name=region_military_enlistment_office)
-    district_military_enlistment_office = post_obj.get("district_military_enlistment_office")
-    district_military_enlistment_office = District.objects.create(region=region_military_enlistment_office, 
-                                                                  district_name=district_military_enlistment_office)
-
-    military_enlishment_name = post_obj.get("military_enlishment_name")
-    military_enlistment_office = MilitaryEnlistmentOffice.objects.create(address=district_military_enlistment_office, 
-                                                                         name=military_enlistment_office)
-    draft_team_name = post_obj.get("draft_team_name")
-    calling_team = CallingTeam.objects.create(name = draft_team_name)
-    front_name = post_obj.get("front_name")
-    army_name = post_obj.get("army_name")
-    warunit_name = post_obj.get("warunit_name")
-    warunits = WarUnit.objects.filter(name__iexact=warunit_name)
-    warunit = None
-    if warunits.count() > 0:
-        warunit = warunits[0]
-    else:
-        WarUnit.objects.create(name=warunit_name, )
-        WarServe.objects.create()
-
-
-    moblization = Mobilization.objects.create(data_mobilization=data_mobilization,
-                                              military_enlistment_office=military_enlistment_office,
-                                              calling_team=calling_team)
-    new_person = Person.objects.create(surname = surname,
-                          surname_distortion = surname_distortion,
-                          name = name, 
-                          name_distortion = name_distortion,
-                          father_name = father_name_distortion)
-    new_person.save()
-
 
 def find_address(places, place_name):
     places = places.objects.filter(region_name__iexact=place_name)
@@ -85,7 +33,68 @@ def fill_address(address):
             is_new = True
 
         addr_prev = addr
-    return addr            
+    return addr       
+
+
+def fill_new_line(post_obj):
+    surname = post_obj.get("surname")
+    surname_distortion = post_obj.get("surname_distortion")
+    name = post_obj.get("name")
+    name_distortion = post_obj.get("name_distortion")
+    father_name = post_obj.get("father_name")
+    father_name_distortion = post_obj.get("father_name_distortion")
+    birthday = post_obj.get("birthday")
+    data_mobilization = post_obj.get("date_mobilization")
+    born_region_name = post_obj.get("born_region_name")
+    born_district_name = post_obj.get("born_district_name")
+    born_locality_name = post_obj.get("born_locality_name")
+    born_address = [born_region_name, born_district_name, born_locality_name]
+    born_locality = fill_address(born_address)
+    live_region_name = post_obj.get("live_region_name")
+    live_district_name = post_obj.get("live_district_name")
+    live_locality_name = post_obj.get("live_locality_name")
+    live_address = [live_region_name, live_district_name, live_locality_name]
+    live_locality = fill_address(live_address)
+    region_military_enlistment_office = post_obj.get("region_region_military_enlistment_office")
+    region_military_enlistment_office = Region.objects.create(region_name=region_military_enlistment_office)
+    district_military_enlistment_office = post_obj.get("district_military_enlistment_office")
+    district_military_enlistment_office = District.objects.create(region=region_military_enlistment_office, 
+                                                                  district_name=district_military_enlistment_office)
+    military_enlishment_name = post_obj.get("military_enlishment_name")
+    military_enlistment_office = MilitaryEnlistmentOffice.objects.create(address=district_military_enlistment_office, 
+                                                                         name=military_enlistment_office)
+    draft_team_name = post_obj.get("draft_team_name")
+    calling_team = CallingTeam.objects.create(name = draft_team_name)
+    front_name = post_obj.get("front_name")
+    army_name = post_obj.get("army_name")
+    warunit_name = post_obj.get("warunit_name")
+    warunits = WarUnit.objects.filter(name__iexact=warunit_name)
+    warunit = None
+    if warunits.count() > 0:
+        warunit = warunits[0]
+    else:
+        WarUnit.objects.create(name=warunit_name, )
+        WarServe.objects.create()
+
+
+    moblization = Mobilization.objects.create(data_mobilization=data_mobilization,
+                                              military_enlistment_office=military_enlistment_office,
+                                              calling_team=calling_team)
+    
+    new_person = Person.objects.create(surname = surname,
+                          surname_distortion = surname_distortion,
+                          name = name, 
+                          name_distortion = name_distortion,
+                          father_name = father_name_distortion,
+                          born_locality = born_locality,
+                          live_locality = live_locality)
+    
+#    war_serve = WarServe(person = new_person,
+#                         war_unit = warunit,
+#                         period_from )
+
+    
+     
 
 
 
--- remember_theirs_by_name_pro/models.py
+++ remember_theirs_by_name_pro/models.py
@@ -23,22 +23,6 @@ class Region(models.Model):
     """
     region_name = models.CharField(max_length=90)
 
-class Person(models.Model):
-    """
-    ФИО, дата рождения, мобилизация, последнее сообщение
-    """
-    name = models.CharField(max_length=30, default='Неизвестно')
-    name_distortion = models.CharField(max_length=30, null=True)
-    surname = models.CharField(max_length=30, default='Неизвестно')
-    surname_distortion = models.CharField(max_length=30, null=True)
-    father_name = models.CharField(max_length=30, null=True)
-    father_name_distortion = models.CharField(max_length=30, null=True)
-    birthday = models.DateField(null=True)
-    born_address_id = models.ForeignKey(Locality, related_name='born', on_delete=models.CASCADE)
-    live_address_id = models.ForeignKey(Locality, related_name='live', on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
-
 
 class WarUnit(models.Model):
     """
@@ -55,29 +39,38 @@ class WarServe(models.Model):
     """
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     war_unit = models.ForeignKey(WarUnit, on_delete=models.CASCADE)
-    period_from = models.DateField(null=False)
-    period_to = models.DateField(null=True)
-
+    
 
 class CallingTeam(models.Model):
+    """
+    Призывная команда
+    """
     name = models.CharField(max_length=30, null=True)
 
 
 class MilitaryEnlistmentOffice(models.Model):
+    """
+    Военкомат
+    """
     address = models.ForeignKey(District, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    mobilizations = models.ManyToManyField(Mobilization, through="Call")
 
 
 class Mobilization(models.Model):
     date_mobilization = models.DateField()
-    military_enlistment_office = models.ForeignKey(MilitaryEnlistmentOffice, null=True, on_delete=models.SET_NULL)
     calling_team = models.ForeignKey(CallingTeam, on_delete=models.CASCADE)
     directed_war_unit = models.ForeignKey(WarUnit, on_delete=models.CASCADE)
     last_message_from_locality = models.ForeignKey(Locality, null=True, on_delete=models.SET_NULL)
 
 
+class Call(models.Model):
+    military_enlistment_office = models.ForeignKey(MilitaryEnlistmentOffice, on_delete=models.CASCADE)
+    mobilization = models.ForeignKey(Mobilization, on_delete=models.CASCADE)
+
+
 class Hospital(models.Model):
-    adress = models.ForeignKey(Locality, on_delete=models.CASCADE)
+    address = models.ForeignKey(Locality, on_delete=models.CASCADE)
     patients = models.ManyToManyField(Person, through='Hospitalization')
     name = models.CharField(max_length=256)
 
@@ -94,12 +87,30 @@ class Hospitalization(models.Model):
 class WarOperation(models.Model):
     name = models.CharField(max_length=256)
     participants = models.ManyToManyField(WarServe, through="WarArchievement")
-    period_from = models.DateField(null=False)
-    period_to = models.DateField(null=False)
     adding_info = models.CharField(max_length=256, null=True)
 
 
 class WarArchievement(models.Model):
     war_operation = models.ForeignKey(WarOperation, on_delete=models.CASCADE)
     war_serve = models.ForeignKey(WarServe, on_delete=models.CASCADE)
+    period_from = models.DateField(null=False)
+    period_to = models.DateField(null=False)
 
+
+
+class Person(models.Model):
+    """
+    ФИО, дата рождения, мобилизация, последнее сообщение
+    """
+    name = models.CharField(max_length=30, default='Неизвестно')
+    name_distortion = models.CharField(max_length=30, null=True)
+    surname = models.CharField(max_length=30, default='Неизвестно')
+    surname_distortion = models.CharField(max_length=30, null=True)
+    father_name = models.CharField(max_length=30, null=True)
+    father_name_distortion = models.CharField(max_length=30, null=True)
+    birthday = models.DateField(null=True)
+    born_locality = models.ForeignKey(Locality, related_name='born', on_delete=models.CASCADE)
+    live_locality = models.ForeignKey(Locality, related_name='live', on_delete=models.CASCADE)
+    call = models.ForeignKey()
+    def __str__(self):
+        return self.name
